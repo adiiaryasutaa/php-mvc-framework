8e423ee945fca66f829664d2137f3afab5c99e0f
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-01-03 21:47:28 +0800

    Add docs

--- src/Ceceply/Framework/Application.php
+++ src/Ceceply/Framework/Application.php
@@ -9,16 +9,35 @@ use Exception;
 
 class Application implements ApplicationContract
 {
+	/**
+	 * The current request
+	 *
+	 * @var Request
+	 */
 	public Request $request;
+
+	/**
+	 * The app router
+	 *
+	 * @var Router
+	 */
 	public Router $route;
 
+	/**
+	 * Application constructor
+	 */
 	public function __construct()
 	{
 		$this->request = new Request();
 		$this->route = new Router($this, $this->request);
 	}
 
-	public function start()
+	/**
+	 * Start application
+	 *
+	 * @return void
+	 */
+	public function start(): void
 	{
 		try {
 			$this->route->resolve($this->request->method(), $this->request->uri());
