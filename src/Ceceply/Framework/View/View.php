29a543807793e40edaa71708af44f7187121f068
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-01-03 21:49:07 +0800

    Add docs and make method

--- src/Ceceply/Framework/View/View.php
+++ src/Ceceply/Framework/View/View.php
@@ -4,15 +4,41 @@ namespace Ceceply\Framework\View;
 
 class View
 {
+	/**
+	 * The content of this view
+	 *
+	 * @var string
+	 */
 	protected string $content;
 
-	public function __construct(string $content)
+	/**
+	 * View constructor
+	 *
+	 * @param string $content
+	 */
+	public function __construct(string $content = '')
 	{
 		$this->content = $content;
 	}
 
-	public function show()
+	/**
+	 * Show content of this view
+	 *
+	 * @return void
+	 */
+	public function show(): void
 	{
 		echo $this->content;
+	}
+
+	/**
+	 * Make new view
+	 *
+	 * @param $content
+	 * @return View
+	 */
+	public static function make($content): View
+	{
+		return (new View($content));
 	}
 }
