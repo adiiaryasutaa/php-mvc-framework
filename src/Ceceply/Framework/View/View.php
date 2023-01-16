69a6d67d24a46159be2333af211ac1791949f014
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-01-16 23:21:03 +0800

    Add view nest

--- src/Ceceply/Framework/View/View.php
+++ src/Ceceply/Framework/View/View.php
@@ -12,6 +12,20 @@ class View
 	protected string $content;
 
 	/**
+	 * The list of nested views
+	 *
+	 * @var View[]
+	 */
+	protected array $nests = [];
+
+	/**
+	 * Is view built?
+	 *
+	 * @var bool
+	 */
+	protected bool $built = false;
+
+	/**
 	 * View constructor
 	 *
 	 * @param string $content
@@ -22,13 +36,17 @@ class View
 	}
 
 	/**
-	 * Show content of this view
+	 * Add nested view
 	 *
-	 * @return void
+	 * @param string $replace
+	 * @param View $view
+	 * @return $this
 	 */
-	public function show(): void
+	public function nest(string $replace, View $view): static
 	{
-		echo $this->content;
+		$this->nests[$replace] = $view;
+
+		return $this;
 	}
 
 	/**
@@ -40,5 +58,30 @@ class View
 	public static function make($content): View
 	{
 		return (new View($content));
+	}
+
+	/**
+	 * Content getter
+	 *
+	 * @return string
+	 */
+	public function getContent(): string
+	{
+		return $this->content;
+	}
+
+	public function getNests(): array
+	{
+		return $this->nests;
+	}
+
+	/**
+	 * Show content of this view
+	 *
+	 * @return string
+	 */
+	public function __toString(): string
+	{
+		return $this->getContent();
 	}
 }
