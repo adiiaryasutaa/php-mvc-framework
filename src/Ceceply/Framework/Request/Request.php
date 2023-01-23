a088c39a256cedc34296ed9bb5091ca02b6174ba
Adi Aryasuta <adiaryasuta.dev@gmail.com>
2023-01-23 22:11:47 +0800

    Add docs in Request.php and changes in uri method

--- src/Ceceply/Framework/Request/Request.php
+++ src/Ceceply/Framework/Request/Request.php
@@ -4,13 +4,23 @@ namespace Ceceply\Framework\Request;
 
 class Request
 {
+	/**
+	 * Get current requested method
+	 *
+	 * @return string
+	 */
 	public function method()
 	{
 		return $_SERVER['REQUEST_METHOD'];
 	}
 
+	/**
+	 * Get current requested uri
+	 *
+	 * @return string
+	 */
 	public function uri()
 	{
-		return $_SERVER['REQUEST_URI'] ?? '/';
+		return trim($_SERVER['REQUEST_URI'], '/');
 	}
 }
